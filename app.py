5cbbff89def0572c37e31e884de28398783fc0e8
ShubhamYadav9416 <103516625+ShubhamYadav9416@users.noreply.github.com>
2022-11-30 16:59:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.write("This program divide 2nd number from first")
-num1 = st.number_input("1st Number")
-num2 = st.number_input("2nd Number")
+num1 = st.number_input("1st Number",min_value=0)
+num2 = st.number_input("2nd Number",min_value=1)
 ans = num1 / num2
 st.write('Result is')
 st.write(ans)
