df876e3a1737d76e5763ca626f6772467736eded
ShubhamYadav9416 <21f1007067@student.onlinedegree.iitm.ac.in>
2022-11-30 16:49:20 +0530

    source code

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit s st
+import pandas as pd
+
+st.write("This program divide 2nd number from first")
+num1 = st.number_input("1st Number")
+num2 = st.number_input("2nd Number")
+ans = num1 / num2
+st.write('Result is')
+st.write(ans)
